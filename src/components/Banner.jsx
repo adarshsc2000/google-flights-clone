61d33f0bf3df3093a0128c97daee372906eb8434
Adarsh Shinju Chandran <adarsh@mannai.tech>
2025-02-05 00:35:18 +0300

    Make banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+const Banner = () => {
+    return (
+        <section className="mx-auto max-w-[1248px] mb-10 relative">
+            <div>
+                <div className="bg-[url(https://www.gstatic.com/travel-frontend/animation/hero/flights_nc_4.svg)] 
+                w-full min-h-[136px] max-h-[298.59375px] h-[23.92578125vw] bg-cover bg-center">
+                </div>
+                <h1 className="block text-center text-[36px] md:text-[56px] leading-[44px] md:leading-[64px] 
+                tracking-normal font-normal absolute left-0 right-0 bottom-0">Flights</h1>
+            </div>
+        </section>
+    )
+}
+
+export default Banner;
